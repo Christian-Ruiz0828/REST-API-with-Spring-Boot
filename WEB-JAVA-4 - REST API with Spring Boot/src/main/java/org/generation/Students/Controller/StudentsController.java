2d8035c3487c1a8a5839d95eceab7cfd2de91052
Christian Ruiz <christian.ruiz0828@gmail.com>
2021-11-10 12:45:17 -0600

    REST APPI WITH SPRING BOOT

---
+++ WEB-JAVA-4 - REST API with Spring Boot/src/main/java/org/generation/Students/Controller/StudentsController.java
@@ -0,0 +1,35 @@
+package org.generation.Students.Controller;
+
+
+import org.generation.Students.StudentsApplication;
+import org.generation.Students.data.entity.Student;
+import org.generation.Students.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class StudentsController {
+
+    private StudentService studentService;
+
+    public StudentsController( @Autowired StudentService studentService )
+    {
+        this.studentService = studentService;
+    }
+
+    @GetMapping( "/student/" )
+    public Iterable<Student> getStudents() {
+        return studentService.all();
+    }
+
+    @GetMapping( "/student/{id}" )
+    public Student getStudent(@PathVariable int id) {
+        return studentService.get(id);
+    }
+
+    @PostMapping("/student")
+    public Student getStudent(@RequestBody ){
+
+    }
+
+}
---
+++ WEB-JAVA-4 - REST API with Spring Boot/src/main/java/org/generation/Students/data/StudentsRepository.java
@@ -0,0 +1,14 @@
+package org.generation.Students.data;
+
+import org.generation.Students.data.entity.Student;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.ArrayList;
+
+public interface StudentsRepository
+    extends CrudRepository<Student, Integer>
+{
+
+    Student findById( int id );
+
+}
